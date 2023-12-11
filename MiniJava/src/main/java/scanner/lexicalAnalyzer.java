cfbef78f62ea931831247278f610cda968823770
hamiddeboo8 <hamiddeh19@gmail.com>
2023-12-11 23:18:26 +0330

    Separate Query From Modifier Refactor

--- MiniJava/src/main/java/scanner/lexicalAnalyzer.java
+++ MiniJava/src/main/java/scanner/lexicalAnalyzer.java
@@ -4,6 +4,7 @@ import errorHandler.ErrorHandler;
 import scanner.token.Token;
 import scanner.type.Type;
 
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,15 +12,28 @@ public class lexicalAnalyzer {
     private Matcher matcher;
 
     public lexicalAnalyzer(java.util.Scanner sc) {
+        StringBuilder input = getStringBuilder(sc);
+        Pattern expression = getPattern();
+        SetMatcher(input, expression);
+    }
+
+    private void SetMatcher(StringBuilder input, Pattern expression) {
+        matcher = expression.matcher(input.toString());
+    }
+
+    private Pattern getPattern() {
+        StringBuilder tokenPattern = new StringBuilder();
+        for (Type tokenType : Type.values())
+            tokenPattern.append(String.format("|(?<%s>%s)", tokenType.name(), tokenType.pattern));
+        return Pattern.compile(tokenPattern.substring(1));
+    }
+
+    private StringBuilder getStringBuilder(Scanner sc) {
         StringBuilder input = new StringBuilder();
         while (sc.hasNext()) {
             input.append(sc.nextLine());
         }
-        StringBuilder tokenPattern = new StringBuilder();
-        for (Type tokenType : Type.values())
-            tokenPattern.append(String.format("|(?<%s>%s)", tokenType.name(), tokenType.pattern));
-        Pattern expression = Pattern.compile(tokenPattern.substring(1));
-        matcher = expression.matcher(input.toString());
+        return input;
     }
 
     public Token getNextToken() {
